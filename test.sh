1fafadb787abcd994833199bf4037052c22a5bd1
j0n1c4 <henintsoahasimanitriniainajoni@gmail.com>
2026-09-24 23:32:12 +0300

    feat: ajout du pipeline Jenkins complet

---
+++ test.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+set -eu  
+# -e: arrête le script si une commande échoue. -u: arrête si une variab
+
+test -f index.html
+grep -q "Application deployee  par Jenkins" index.html
+
+echo "Test reussi : index.html est ouvert"
