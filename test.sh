28ef1bab5e965b070388c38d15a9cecdc2a15c90
j0n1c4 <henintsoahasimanitriniainajoni@gmail.com>
2026-09-25 11:51:38 +0300

    fix (test.sh) phrase  a rechercher

--- test.sh
+++ test.sh
@@ -3,6 +3,6 @@ set -eu
 # -e: arrête le script si une commande échoue. -u: arrête si une variab
 
 test -f index.html
-grep -q "Application deployee  par Jenkins" index.html
+grep -q "Application déployée par Jenkins" index.html
 
 echo "Test reussi : index.html est ouvert"
